1bdca4c69bd886d39b9b8fc030798462d5dd0fce
alumno <fouad>
2022-10-27 10:58:10 +0200

    resources

--- app/src/main/java/org/fouad/appandroid/MainActivity.java
+++ app/src/main/java/org/fouad/appandroid/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.material.circularreveal.CircularRevealHelper;
 
 import org.fouad.appandroid.Clases.User;
 import org.fouad.appandroid.ConnectDatabase.Model;
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         txtapellido=findViewById(R.id.textApellido);
         txtPassword=findViewById(R.id.textPassword);
         txtEmail=findViewById(R.id.textAddress);
+        //String veces=getString(R.string.vecesPulsado);
+       // Resources resources=getResources(); mirar pdf alberto y apuntes alba
+        //String nombre=resources.getQuantityString("");
     }
 
     public void ClicktoPage1(View view) {
